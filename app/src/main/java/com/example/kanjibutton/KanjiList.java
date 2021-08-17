dc1d72a5e7a2f01ae3c62b8e316571477dd938d7
KEVIN LOTAN <klotan@fmi.com>
2021-08-18 01:13:38 +0700

    add popup setting template

--- app/src/main/java/com/example/kanjibutton/KanjiList.java
+++ app/src/main/java/com/example/kanjibutton/KanjiList.java
@@ -14,7 +14,11 @@ public class KanjiList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kanji_list);
+        View blurScreen = findViewById(R.id.blur);
         ImageView btnBack = findViewById(R.id.btnBack);
+        View settingBar = findViewById(R.id.popupSetting);
+        blurScreen.setVisibility(View.GONE);
+        settingBar.setVisibility(View.GONE);
         btnBack.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -26,8 +30,15 @@ public class KanjiList extends AppCompatActivity {
         btnSettings.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Intent newIntent = new Intent(KanjiList.this, Setting.class);
-                startActivity(newIntent);
+                settingBar.setVisibility(View.VISIBLE);
+                blurScreen.setVisibility(View.VISIBLE);
+            }
+        });
+        blurScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                settingBar.setVisibility(View.GONE);
+                blurScreen.setVisibility(View.GONE);
             }
         });
     }
